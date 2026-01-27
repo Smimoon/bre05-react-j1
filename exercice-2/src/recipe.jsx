c85274ac4c6194ecb06e0217e4e347a368f387e1
smimoon <simonlaroche@orange.fr>
2026-01-27 12:37:12 +0000

    Save end exercice-2

---
+++ exercice-2/src/recipe.jsx
@@ -0,0 +1,14 @@
+const Recipe = (props) => {
+    const ingredients = ["Oeufs", "Lait", "Farine", "Beurre"];
+    return (
+        <article>
+            <h2>La recette de la pâte à crêpes</h2>
+            <h3>Ingrédients</h3>
+            <ul>
+                {ingredients.map((ingredient, i) => <li key={i}>{ingredient}</li>)}
+            </ul>
+        </article>
+        );
+};
+
+export default Recipe; 
